369ba93e0e480a49eace83742cdfe6e6606a78f4
Armen <armentovmasyan02@gmail.com>
2019-11-24 23:28:01 +0300

    Добавил класс Sort

---
+++ src/main/java/Classes/PersonRepositorySort.java
@@ -0,0 +1,33 @@
+package Classes;
+import java.util.Comparator;
+
+import ru.vsu.lab.entities.IPerson;
+import  ru.vsu.lab.repository.*;
+
+public class PersonRepositorySort {
+	
+	public static IPersonRepository bubbleSort(Comparator<IPerson> comparator,IPersonRepository rep,int size)
+	{
+		IPersonRepository rep2=rep;
+		boolean needIteration = true;
+		while (needIteration) {
+			needIteration = false;
+
+		for (int i = 1; i < size; i++) {
+			if (comparator.compare(rep2.get(i),rep.get(i-1))<0) {	
+				swap(rep2, i, i-1);
+				needIteration = true;
+
+				}
+		 	}	
+		}
+		
+		return rep2;
+	}
+	
+	private static void swap(IPersonRepository rep, int index1, int index2) {
+		IPerson tmp = rep.get(index1);
+		rep.set(index1, rep.get(index2));
+		rep.set(index2,tmp);
+	}
+}
